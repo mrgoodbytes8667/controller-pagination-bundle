902443ab66c2294a1d2f54aa252145e6f89864c1
MrGoodBytes <bytes@goodbytes.live>
2022-02-22 14:25:42 -0600

    In PaginationHelper, rename parameterAllowlist to parametersAllowlist, begin to start.

--- src/DependencyInjection/BytesControllerPaginationExtension.php
+++ src/DependencyInjection/BytesControllerPaginationExtension.php
@@ -25,15 +25,20 @@ class BytesControllerPaginationExtension extends Extension implements ExtensionI
 
         $configuration = $this->getConfiguration($configs, $container);
 
+        /** @var array $config = ['offsets' => ['start' => 1, 'begin' => 1, 'end' => 1, 'current' => 1], 'parameters_allowlist' => [], 'parameter_allowlist' => []] */
         $config = $this->processConfiguration($configuration, $configs);
 
         $allowlist = [];
-        if (array_key_exists('parameter_allowlist', $config)) {
+        if (array_key_exists('parameters_allowlist', $config)) {
+            $allowlist = $config['parameters_allowlist'];
+        } elseif (array_key_exists('parameter_allowlist', $config)) {
             $allowlist = $config['parameter_allowlist'];
         }
 
+        $start = array_key_exists('start', $config['offsets']) ? $config['offsets']['start'] : $config['offsets']['begin'];
+
         $definition = $container->getDefinition('bytes_controller_pagination.pagination');
-        $definition->replaceArgument(1, $config['offsets']['begin']);
+        $definition->replaceArgument(1, $start);
         $definition->replaceArgument(2, $config['offsets']['end']);
         $definition->replaceArgument(3, $config['offsets']['current']);
         $definition->replaceArgument(4, $allowlist);
