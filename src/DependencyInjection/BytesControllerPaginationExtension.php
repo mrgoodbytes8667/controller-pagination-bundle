cf2a1b525e209c371ae74176e590008892718130
MrGoodBytes <bytes@mrgoodbytes.dev>
2026-06-22 14:46:26 -0500

    Fix missing return type

--- src/DependencyInjection/BytesControllerPaginationExtension.php
+++ src/DependencyInjection/BytesControllerPaginationExtension.php
@@ -18,7 +18,7 @@ class BytesControllerPaginationExtension extends Extension implements ExtensionI
      * @param ContainerBuilder $container
      * @throws Exception
      */
-    public function load(array $configs, ContainerBuilder $container)
+    public function load(array $configs, ContainerBuilder $container): void
     {
         $loader = new PhpFileLoader($container, new FileLocator(__DIR__ . '/../Resources/config'));
         $loader->load('services.php');
